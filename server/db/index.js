82268ba459509a0837b21ae7d41e5788e783f883
Joseph Stalnaker <jstalnaker@JosephSComputer.lan>
2021-06-15 16:52:09 -0400

    completed furniture model

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,18 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require('./db');
 
-const User = require('./models/User')
+const User = require('./models/User');
+const Furniture = require('./models/Furniture');
 
 //associations could go here!
+// Furniture.belongsTo(Manufacture);
+// Manufacture.hasMany(Furniture);
 
 module.exports = {
   db,
   models: {
     User,
+    Furniture,
   },
-}
+};
