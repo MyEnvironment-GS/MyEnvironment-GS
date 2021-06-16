49bbe5cb70443468d03457c1881ba7f343898b4d
Michael Baez <mrbaez427@gmail.com>
2021-06-16 13:02:41 -0400

    Bugfixes

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -37,15 +37,15 @@ export class AllProducts extends Component {
           {furniture.map(furniture => {
             return (
               <li key={furniture.id}>
-                <div class='row'>
-                  <div class='col s12 m7'>
-                    <div class='card'>
-                      <div class='card-image'>
+                <div className='row'>
+                  <div className='col s12 m7'>
+                    <div className='card'>
+                      <div className='card-image'>
                         <img src={furniture.imageUrl}></img>
-                        <span class='card-title'>{furniture.name}</span>
+                        <span className='card-title'>{furniture.name}</span>
                       </div>
-                      <div class='card-content'>
-                        <p>{`Price: ${furniture.price}`}</p>
+                      <div className='card-content'>
+                        <p>{`Price: $${furniture.price}`}</p>
                       </div>
                     </div>
                   </div>
