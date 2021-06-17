dffcae099f18a81817b59b30fe96cdae2a2e2b70
Joseph Stalnaker <jstalnaker@JosephSComputer.lan>
2021-06-17 00:09:17 -0400

    working on NavBar

---
+++ client/components/Menu.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+export default function SimpleMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        Menu Icon
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <Link to="/home">
+          <MenuItem onClick={handleClose}>HOME</MenuItem>
+        </Link>
+        <MenuItem onClick={handleClose}>ALL PRODUCTS</MenuItem>
+        <MenuItem onClick={handleClose}>ABOUT US</MenuItem>
+        <MenuItem onClick={handleClose}>SIGN OUT</MenuItem>
+        <MenuItem onClick={handleClose}>ADMIN</MenuItem>
+      </Menu>
+    </div>
+  );
+}
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,47 +1,51 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../store';
+import SimpleMenu from './Menu';
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
-    <h1>FS-App-Template</h1>
     <nav>
+      <h1 className="nav-bar-header">ENVIRONMENT</h1>
       {isLoggedIn ? (
-        <div>
+        <div className="logged-in-links">
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
+          <Link to="/cart">Cart</Link>
+          <SimpleMenu />
           <a href="#" onClick={handleClick}>
             Logout
           </a>
         </div>
       ) : (
-        <div>
+        <div className="not-logged-in-links">
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
+          <Link to="/cart">Cart</Link>
           <Link to="/signup">Sign Up</Link>
+          <SimpleMenu />
         </div>
       )}
     </nav>
-    <hr />
   </div>
-)
+);
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    isLoggedIn: !!state.auth.id
-  }
-}
+    isLoggedIn: !!state.auth.id,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleClick() {
-      dispatch(logout())
-    }
-  }
-}
+      dispatch(logout());
+    },
+  };
+};
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(Navbar);
