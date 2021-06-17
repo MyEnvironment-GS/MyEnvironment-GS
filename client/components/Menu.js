d3c2dd5952fedbc48d8934242f8bc344fcc36106
Joseph Stalnaker <jstalnaker@JosephSComputer.lan>
2021-06-17 09:43:07 -0400

    adjusting layout of component

--- client/components/Menu.js
+++ client/components/Menu.js
@@ -3,6 +3,21 @@ import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import { withStyles } from '@material-ui/core/styles';
+
+// 1. We define the styles.
+const styles = (theme) => ({
+  root: {
+    color: 'inherit',
+    textDecoration: 'inherit',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+  primary: {
+    color: theme.palette.primary.main,
+  },
+});
 
 export default function SimpleMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -31,13 +46,15 @@ export default function SimpleMenu() {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <Link to="/home">
-          <MenuItem onClick={handleClose}>HOME</MenuItem>
-        </Link>
-        <MenuItem onClick={handleClose}>ALL PRODUCTS</MenuItem>
-        <MenuItem onClick={handleClose}>ABOUT US</MenuItem>
-        <MenuItem onClick={handleClose}>SIGN OUT</MenuItem>
-        <MenuItem onClick={handleClose}>ADMIN</MenuItem>
+        <div className="button-link">
+          <Link to="/home">
+            <MenuItem onClick={handleClose}>HOME</MenuItem>
+          </Link>
+          <MenuItem onClick={handleClose}>ALL PRODUCTS</MenuItem>
+          <MenuItem onClick={handleClose}>ABOUT US</MenuItem>
+          <MenuItem onClick={handleClose}>SIGN OUT</MenuItem>
+          <MenuItem onClick={handleClose}>ADMIN</MenuItem>
+        </div>
       </Menu>
     </div>
   );
