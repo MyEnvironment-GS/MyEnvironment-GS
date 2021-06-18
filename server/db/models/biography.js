99c2571441448785eb5ed8de69281bb06efb5592
Joseph Stalnaker <jstalnaker@JosephSComputer.lan>
2021-06-18 12:23:59 -0400

    completed redux cycle for About

--- server/db/models/biography.js
+++ server/db/models/biography.js
@@ -9,18 +9,18 @@ const Biography = db.define('biography', {
     validate: {
       notEmpty: true,
     },
-    description: {
-      type: Sequelize.TEXT,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
     },
-    imageUrl: {
-      type: Sequelize.TEXT,
-      defaultValue:
-        'https://cdn1.vectorstock.com/i/thumb-large/77/30/default-avatar-profile-icon-grey-photo-placeholder-vector-17317730.jpg',
-    },
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    defaultValue:
+      'https://cdn1.vectorstock.com/i/thumb-large/77/30/default-avatar-profile-icon-grey-photo-placeholder-vector-17317730.jpg',
   },
 });
 
