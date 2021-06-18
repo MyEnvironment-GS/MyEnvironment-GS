97a635b28d614364e67fb550702342b293e1ccbb
Cody Swithenbank <cody.swithenbank@gmail.com>
2021-06-18 10:56:38 -0700

    cart bugfixes and Route bugfix

--- client/Routes.js
+++ client/Routes.js
@@ -8,6 +8,7 @@ import { me } from './store';
 import AllProducts from './components/AllProducts';
 import SingleProduct from './components/SingleProduct';
 import Cart from './components/Cart';
+import About from './components/About'
 
 /**
  * COMPONENT
