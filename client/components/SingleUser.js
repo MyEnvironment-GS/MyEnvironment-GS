85f46166a4280d14c41f5e4ef56639edc1c1f5c7
Michael Gant <your_email_here>
2021-06-21 14:58:40 -0400

    build singleUserComponent

---
+++ client/components/SingleUser.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { fetchSingleUser } from '../store/effects/users'
+
+export  class SingleUser extends Component {
+  componentDidMount() {
+    try {
+      const userId = this.props.match.params.userId;
+      this.props.loadSingleUser(userId)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  render() {
+    const singleUser = this.props.singleUser
+    const firstName = singleUser.firstName
+    console.log(singleUser)
+    return (
+      <div>
+       <h1>{firstName}</h1>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+ singleUser: state.singleUser
+})
+
+
+const mapDispatchToProps = (dispatch) => ({
+  loadSingleUser: (id) => dispatch(fetchSingleUser(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleUser)
