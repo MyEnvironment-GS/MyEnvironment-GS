f67ed3ee39cd521cbff46bbd3b033a6cdab3726a
Joseph Stalnaker <jstalnaker@JosephSComputer.lan>
2021-06-22 22:17:23 -0400

    working on AllProductsAdmin

--- client/components/Admin.js
+++ client/components/Admin.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import AllProductsAdmin from './AllProductsAdmin';
 import Users from './Users';
 
 export default function Admin() {
   return (
-    <div>
+    <div className="admin-container">
       <div>
         <h2>Users</h2>
         <div>
@@ -13,6 +14,9 @@ export default function Admin() {
         </div>
       </div>
       <h2>Products</h2>
+      <div>
+        <AllProductsAdmin />
+      </div>
     </div>
   );
 }
